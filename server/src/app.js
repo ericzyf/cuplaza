0a76f2560b07e76ba96f0d223784cd02693427b4
Yifan Zhang <yfzhang1999@gmail.com>
2019-04-05 03:51:57 +0800

    backend api init

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+const mongodb = require('mongodb')
 const morgan = require('morgan')
 
 const app = express()
@@ -8,11 +9,18 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send({
-    msg: 'test'
-  })
+async function loadCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb+srv://admin:admin@cuplaza-api-tooxw.mongodb.net/test?retryWrites=true', { useNewUrlParser: true})
+  return client.db('api').collection('items')
+}
+
+app.get('/api', async (req, res) => {
+  const data = await loadCollection()
+  const json = await data.find().toArray()
+  console.log(json)
+  res.send(json)
 })
 
-console.log('App listening on port 8081')
-app.listen(8081)
+app.listen(8081, () => {
+    console.log('app listening on port 8081...')
+})
