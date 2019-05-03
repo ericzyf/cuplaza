951c99a4b8b350f722155ab36154555147b60838
Yifan Zhang <yfzhang1999@gmail.com>
2019-05-03 10:08:15 +0800

    comment

--- client/src/api/SearchService.js
+++ client/src/api/SearchService.js
@@ -2,9 +2,18 @@ const axios = require('axios')
 
 const url = 'http://localhost:8081/api/search/'
 
+/**
+ * This is the search service api.
+ * -------------------------------
+ * This component handles all the requests related to
+ * the search system. It can be used to search for
+ * items that match with the search keyword.
+ */
+
 class SearchService {
   /**
    * make http GET request
+   * retrieve the information of all the items that match with the search keyword
    * @param b64 base64url-encoded search keyword
    */
   static searchItem(b64) {
