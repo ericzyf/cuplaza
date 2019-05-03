085db9c6f9d2c21d5c51d3acaa87dffd516c9a0d
Yifan Zhang <yfzhang1999@gmail.com>
2019-05-03 09:46:29 +0800

    comment

--- client/src/api/ItemService.js
+++ client/src/api/ItemService.js
@@ -2,6 +2,16 @@ const axios = require('axios')
 
 const url = 'http://localhost:8081/api/items/'
 
+/**
+ * This is the item service api.
+ * -----------------------------
+ * This component handles all the requests related to
+ * the item management system. It can be used to
+ * retrieve all the records in the item database,
+ * create a new record, as well as delete a record in
+ * the item database.
+ */
+
 class ItemService {
   /**
    * make http GET request
@@ -20,6 +30,7 @@ class ItemService {
 
   /**
    * make http POST request
+   * create a new record in 'items' collection
    * @param item item info in json
    */
   static postItem(item) {
@@ -28,6 +39,7 @@ class ItemService {
 
   /**
    * make http DELETE request
+   * delete a record in 'items' collection
    * @param id id of the item to be deleted
    */
   static deleteItem(id) {
